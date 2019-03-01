b21f34e6ab8b172f47bf4467068808fddec11b95
Filipe Maldonado <filipeamaldonado@gmail.com>
2019-03-01 04:49:01 +0000

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+// Trocar imagem
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox_logo.png') {
+    myImage.setAttribute ('src','images/firefox_logo2.png');
+  } else {
+    myImage.setAttribute ('src','images/firefox_logo.png');
+  }
+}
+
+// Mensagem de boas vindas inicial
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+}
+else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
